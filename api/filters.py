5d130d1856d3872f68bc29ac68e14d52be763044
Семен Пичугин <pichugin.semen@gmail.com>
2020-11-03 23:13:38 +0300

    After first review

--- api/filters.py
+++ api/filters.py
@@ -4,7 +4,7 @@ from .models import Title
 
 
 class TitleFilter(filters.FilterSet):
-    """***"""
+    """Фильтр по указанным полям, который применяем для поиска по БД"""
 
     genre = filters.CharFilter(
         field_name="genre__slug"
